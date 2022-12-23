2ea4228a6b544fea697d165b39ce02c8c2f983fd
anitha-a12 <anitha.a1294@gmail.com>
2022-12-23 15:25:29 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+class Quiz extends React.Component {
+ render()
+ {
+ return <div>hello</div>
+ }
+
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Quiz/>);
