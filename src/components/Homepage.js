7070bf0a1743f72c975f66c8ccabd7f3bafbb39c
Swayambhu Prasad Panda <swayambhuprasadpanda@gmail.com>
2022-12-24 10:58:29 +0530

    Home page and redux lib added.

---
+++ src/components/Homepage.js
@@ -0,0 +1,17 @@
+
+import '../style.css'
+function Home() {
+    return (
+        <div id="body">
+            <div id="box">
+                <h2>Welcome!</h2>
+                <div>
+                    <input placeholder='Enter name'></input>
+                    <button>Submit</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import App from './app';
 
-class Quiz extends React.Component {
- render()
- {
- return <div>hello</div>
- }
-
-
-}
-
+const store = configureStore({
+    reducer: {},
+})
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Quiz/>);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
